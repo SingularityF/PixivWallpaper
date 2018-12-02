783e898255d9aed2a78fb4d0eb803659f2b14693
SingularityF <mailtosingularityf@gmail.com>
2018-12-02 02:18:06 -0500

    Add handler for demo

--- Server/select_paper.php
+++ Server/select_paper.php
@@ -11,16 +11,23 @@ $pdo = new PDO($dsn, $user, $pass);
 #$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $query=$pdo->prepare("SELECT A.*, B.Score FROM images_l A, (SELECT *,(AvgGradient-POW(7*ABS(AspectRatio-?),2)-20*Variance) AS Score FROM todays_best ORDER BY Score DESC LIMIT 1) B WHERE A.IllustID=B.IllustID");
+$query_demo=$pdo->prepare("SELECT *,(AvgGradient-POW(7*ABS(AspectRatio-?),2)-20*Variance) AS Score FROM todays_best ORDER BY Score DESC LIMIT 1");
 
 $ratio=$_POST["ar"];
+$demo=$_POST["demo"];
 
 if(is_null($ratio)){
     die("No post parameter detected");
 }
 
-$query->execute(array($ratio));
 
-$res=$query->fetch(PDO::FETCH_ASSOC);
+if(is_null($demo)){
+	$query->execute(array($ratio));
+	$res=$query->fetch(PDO::FETCH_ASSOC);
+}else{
+	$query_demo->execute(array($ratio));
+	$res=$query_demo->fetch(PDO::FETCH_ASSOC);
+}
 
 #echo $res["Checksum"];
 
