b01edd08d17e0284b0ad23ccd7370c6267da39d9
SingularityF <mailtosingularityf@gmail.com>
2018-12-16 20:08:22 -0600

    Add waifu2x to enlarge wallpaper

--- Client/set_wallpaper.py
+++ Client/set_wallpaper.py
@@ -3,37 +3,57 @@ import os
 import sys
 import win32api, win32con, win32gui
 import tkinter
+from subprocess import call
+from uuid import getnode
+
+url="https://singf.space/pixiv/select_paper.php"
 
 def pnf(msg):
     print(msg)
     sys.stdout.flush()
 
-url="https://singf.space/pixiv/select_paper.php"
-
-def setWallpaper(path):
+def set_wallpaper(path):
     key = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER,"Control Panel\\Desktop",0,win32con.KEY_SET_VALUE)
     win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, path, 1+2)
 
-pnf("Gathering machine information...")
-# May not work for multiple monitor setups
-root = tkinter.Tk()
-width = root.winfo_screenwidth()
-height = root.winfo_screenheight()
-
-pnf("Fetching wallpaper...")
-try:
-    r = requests.post(url, data = {"ar":float(width)/float(height)})
-except:
-    pnf("Download failed, check your internet connection")
-    input("Press any key to quit...")
-    quit()
-
-ext=r.headers["Content-Type"].split("/")[1]
-output_file="wallpaper.{}".format(ext)
-
-with open(output_file,"wb") as f:
-    f.write(r.content)
+def download_and_set(enlarge=False):
+    pnf("Gathering machine information...")
+    # May not work for multiple monitor setups
+    root = tkinter.Tk()
+    width = root.winfo_screenwidth()
+    height = root.winfo_screenheight()
+    root.destroy()
     
-pnf("Setting wallpaper...")
-path = os.path.abspath(output_file)
-setWallpaper(path)
+    pnf("Fetching wallpaper...")
+    uuid=str(getnode())
+    try:
+        r = requests.post(url, data = {"ar":float(width)/float(height),
+            "uuid":uuid})
+    except:
+        return (1,"")
+            
+    ext=r.headers["Content-Type"].split("/")[1]
+    output_file="wallpaper.{}".format(ext)
+    
+    with open(output_file,"wb") as f:
+        f.write(r.content)
+
+    if enlarge==True:
+        pnf("Enlarging wallpaper...")
+        path_waifu2x = os.path.abspath("waifu2x-caffe/waifu2x-caffe-cui.exe")
+        path_input=os.path.abspath(output_file)
+        path_output=os.path.abspath("wallpaper2x.{}".format(ext))
+        call([path_waifu2x,"-i",path_input,"-o",path_output,"-p","cpu","-m","scale"])
+        output_file="wallpaper2x.{}".format(ext)
+        
+    pnf("Setting wallpaper...")
+    path = os.path.abspath(output_file)
+    set_wallpaper(path)
+    return (0,output_file)
+
+if __name__ == "__main__":
+    status,_=download_and_set()
+    if status==1:
+        pnf("Download failed, check your internet connection")
+        input("Press any key to quit...")
+        quit()
