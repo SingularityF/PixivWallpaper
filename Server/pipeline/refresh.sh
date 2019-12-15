fef58985a2b6aa9fa95b864a813fa9f99acc12d2
SingularityF <mailtosingularityf@gmail.com>
2019-12-15 01:52:51 +0000

    Add code to identify successful download

--- Server/pipeline/refresh.sh
+++ Server/pipeline/refresh.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-python3 download.py && php upload.php
-kill `pgrep Xvfb`
-kill `pgrep geckodriver`
+python3 download.py
+if [ $? -eq 0 ]
+then
+  echo "Download successful, continuing"
+  kill `pgrep Xvfb`
+  kill `pgrep geckodriver`
+else
+  echo "Download failed, exiting" >&2
+  kill `pgrep Xvfb`
+  kill `pgrep geckodriver`
+  exit 1
+fi
+php upload.php
